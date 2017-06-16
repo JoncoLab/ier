9ad9afe90e5db4443965920eaaef8e2df81a8b83
Saladin <joncolab@gmail.com>
2017-06-16 04:14:44 +0300

    1.2.3
    exclude build/ in .gitignore
    styles for header

--- src/scripts/js/common.js
+++ src/scripts/js/common.js
@@ -1,3 +1,14 @@
 /**
  * Created by Saladin on 13.06.2017.
  */
+var main = function () {
+    var navigationBar = $('.main-menu'),
+        menuItems = $('.menu-item, .drop-down-menu-item'),
+        dropDownButton = $('#info');
+
+    dropDownButton.click(function () {
+        var currentMenuItem = $(this);
+        currentMenuItem.toggleClass('open');
+    });
+};
+$(document).ready(main);
